faac1cbec7bc81dff1007d3bc9c00dadab3f1ead
BrayanCahuana <cahuanabrayan123@gmail.com>
2021-05-31 20:53:13 -0500

    no hay nada

--- dia1/02-encapsulamiento.py
+++
@@ -1,8 +0,0 @@
-class Vehiculo:
-    def __init__(self , largo, ancho,motor, enMarcha=False):
-        self.largo = largo
-        self.ancho = ancho
-        self.motor = motor
-        self.enMarcha = enMarcha
-
-        
